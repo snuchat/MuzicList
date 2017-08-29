a7a70188703c5b347be6683c72166438d83220d5
snuchat <snuchat@gmail.com>
2017-08-30 00:29:02 +0900

    change model and display user profile in index.html

--- src/myaccount/models.py
+++ src/myaccount/models.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+	#it is not wise to user User model directly from django.contrib.auth.models 
+	#it is wise to user this way
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
 	nickname = models.CharField(max_length=30, unique=True)
 
 	def __str__(self):
